8ad805762234d42bfe85b9751073ee85faf55027
John Tucker <jtucker@spotify.com>
2022-04-17 14:01:52 -0400

    basic map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "./App.css";
 
 function App() {
   return (
-    <div>Hello World</div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{height: '500px'}}>
+      <TileLayer
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
