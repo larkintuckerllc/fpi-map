9901e8f3c2e80d5f3ee61ac6d00ee5b83605982d
John Tucker <jtucker@spotify.com>
2022-04-17 20:59:03 -0400

    close

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Indicator from './Indicator';
 import "./App.css";
+import styles from './App.module.css';
 
 function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{height: '500px'}}>
-      <TileLayer
-        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-        url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          <Indicator
-            community={2}
-            ecological={3}
-            economic={4}
-            image="http://localhost:3000/1.jpeg"
-            link="https://www.google.com"
-            name="testing one two three, testing"
-          />
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className={styles.root}>
+      <div className={styles.root__title}>FPI Listing</div>
+      <MapContainer center={[0, 0]} zoom={3} className={styles.root__map}>
+        <TileLayer
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        />
+        {window.fpiIndicators.map(indicator => (
+          <Marker key={indicator.id} position={[indicator.latitude, indicator.longitude]}>
+            <Popup>
+              <Indicator
+                community={indicator.community}
+                ecological={indicator.ecological}
+                economic={indicator.economic}
+                image={indicator.image}
+                link={indicator.link}
+                name={indicator.name}
+              />
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
 
 export default App;
+
+//       window.fpiIndicators = [{"id":11,"link":"http:\/\/localhost:8000\/fpi_indicator\/11\/","name":"Debug Name 3","image":"http:\/\/localhost:3000\/1.jpg","latitude":"0","longitude":"-7","ecological":"3","economic":"3","community":"3"}];
